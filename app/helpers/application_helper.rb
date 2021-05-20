4b8064cce9dd8fe838e77fdeef602884ba304acc
Cassio Almeida <jcassiocosta@gmail.com>
2021-05-20 11:49:45 -0300

    locale and default pages WIP

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,16 @@
 module ApplicationHelper
+  def nav_bar
+    content_tag(:ul, class: 'nav col-12 col-lg-auto me-lg-auto mb-2 justify-content-center mb-md-0') do 
+      yield
+    end
+  end
+  
+  def nav_link(text, path)
+    options = current_page?(path) ? { class: 'nav-link px-2 text-secondary' } : { class: 'nav-link px-2 text-white' }
+    content_tag(:li) do 
+      link_to(path, options) do 
+        text.html_safe
+      end
+    end
+  end
 end
