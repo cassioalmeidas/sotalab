e7555ca10d636be42320f5f495030130d642fbb1
Cassio Almeida <jcassiocosta@gmail.com>
2021-06-19 10:23:15 -0300

    rerun webpacker:install:erb command in an attempt to fix deploy heroku

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,7 +1,6 @@
 const { environment } = require('@rails/webpacker')
 const erb = require('./loaders/erb')
 
-environment.loaders.prepend('erb', erb)
 
 const webpack = require('webpack')
 environment.plugins.prepend('Provide',
@@ -11,4 +10,5 @@ environment.plugins.prepend('Provide',
   })
 )
 
+environment.loaders.prepend('erb', erb)
 module.exports = environment
